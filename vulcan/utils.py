c7f12a5143442c3722f036ef3fb37288de5e11d7
Kacper Ziubryniewicz <kapi2289@gmail.com>
2018-12-12 19:49:23 +0100

    Fix OpenSSL DeprecationWarning

--- vulcan/utils.py
+++ vulcan/utils.py
@@ -20,6 +20,6 @@ def find(_list, key, value):
     return next((i for i in _list if i[key] == value), None)
 
 def signature(cert, passphrase, data):
-    p12 = crypto.load_pkcs12(base64.b64decode(cert), passphrase)
-    sign = crypto.sign(p12.get_privatekey(), json.dumps(data), 'RSA-SHA1')
+    p12 = crypto.load_pkcs12(base64.b64decode(cert), passphrase.encode('utf-8'))
+    sign = crypto.sign(p12.get_privatekey(), json.dumps(data).encode('utf-8'), 'RSA-SHA1')
     return base64.b64encode(sign).decode('utf-8')
