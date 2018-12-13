091b9aca3816e3fcd175ff3f214006a0d3a35b42
Kacper Ziubryniewicz <kapi2289@gmail.com>
2018-12-13 23:14:22 +0100

    Version up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Kacper Ziubryniewicz'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from .vulcan import *
 
 __title__ = 'vulcan'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __description__ = 'API for Vulcan e-journal'
 __doc__ = __description__
 
