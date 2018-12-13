e32355bdc483f24f82d45591b408aa5a4b6cee28
Kacper Ziubryniewicz <kapi2289@gmail.com>
2018-12-13 23:05:34 +0100

    Update version in docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Kacper Ziubryniewicz'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
