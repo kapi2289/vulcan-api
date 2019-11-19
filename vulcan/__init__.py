087485d4d863c4d993b6b6e2c24a5a39add0a429
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-11-19 21:11:42 +0100

    Bump version to 1.0.0

--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from ._vulcan import Vulcan
 
-__version__ = "0.3.1"
+__version__ = "1.0.0"
 __doc__ = "Nieoficjalne API do dzienniczka elektronicznego UONET+"
 
 __all__ = ["Vulcan"]
