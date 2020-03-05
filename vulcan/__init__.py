31509f6e11ce346414721b8c7bccac387cda740f
Kacper Ziubryniewicz <kapi2289@gmail.com>
2020-03-05 15:42:01 +0100

    Bump version to 1.0.3

--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from ._vulcan import Vulcan
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __doc__ = "Unofficial API for UONET+ e-register"
 
 __all__ = ["Vulcan"]
