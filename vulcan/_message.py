d5182043e404410aa21542c21fc3c96599010940
HunterAzar <0tobik09@gmail.com>
2020-03-27 15:30:22 +0100

    Messages v!.0.1

--- vulcan/_message.py
+++ vulcan/_message.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from datetime import datetime
 
 from related import (
+	TimeField,
     IntegerField,
     StringField,
     DateField,
@@ -14,38 +17,25 @@ from ._teacher import Teacher
 from ._utils import sort_and_filter_date
 @immutable
 class Message:
-	sender = StringField(key="Nadawca")
-	senderid = StringField(key="NadawcaId")
-	addressees = StringField(key="Adresaci")
+	id = IntegerField(key="WiadomoscId")
+	sender_id = StringField(key="NadawcaId")
+	recipients = StringField(key="Adresaci")
 	title = StringField(key="Tytul")
 	content = StringField(key="Tresc")
-	postdate = StringField(key="DataWyslania")
-	posthour = StringField(key="GodzinaWyslania")
+	sent_data = StringField(key="DataWyslania")
+	sent_time = StringField(key="GodzinaWyslania")
 	folder= StringField(key="FolderWiadomosci")
-	"""
-	messageid = ChildField(MessageId, required=False)
-	"""
-	senderid = ChildField(Teacher, required=False)
+	sender = ChildField(Teacher, required=False)
 	@classmethod
 	def get(cls, api, date=None):
 		if not date:
-			date = datetime.now()
-		date_str = date.strftime("%Y-%m-%d")
+			date=api.student.period.from_
+		date_first=date.strftime("%Y-%m-%d")
+		date_last=api.student.period.to.strftime("%Y-%m-%d")
+		data = {"DataPoczatkowa": date_first, "DataKoncowa": date_last}
+		j = api.post("Uczen/WiadomosciOdebrane", json=data) 
+		messages=j.get("Data", [])
 
-		date = {"DataPoczatkowa": "2020-01-01", "DataKoncowa": date_str}
-		j = api.post("Uczen/WiadomosciOdebrane", json=date) 
-		s1=j.get("Data", [])
-
-		for message in s1:
+		for message in messages:
 			message["sender"] = api.dict.get_teacher(message["NadawcaId"])
 			yield to_model(cls, message)
-	"""
-	@classmethod
-	def send(cls, api, name, title, content, adresses):
-		 
-		mesage={"NadawcaWiadomosci":name,"Tytul":title, "Tresc":content, "Adresaci":[{"Nazwa":adresses}]}
-		print(name+title+content+adresses)
-		print(name)
-		sendmessage=api.post("Uczen/DodajWiadomosc", json=mesage, as_json=False)
-		print(sendmessage)
-	"""
