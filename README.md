81d9065578e841846ce88a570125b4978b0b79ad
HunterAzar <0tobik09@gmail.com>
2020-03-27 16:47:34 +0100

    Messages v1.0.2

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-<h1 align="center">Vulcan API </h1>
+<h1 align="center">Vulcan API</h1>
 <p align="center">
     <a href="https://github.com/kapi2289/vulcan-api/blob/master/LICENSE"><img src="https://img.shields.io/pypi/l/vulcan-api.svg" alt="Licencja"></a>
     <a href="https://pypi.org/project/vulcan-api/"><img src="https://img.shields.io/pypi/v/vulcan-api.svg" alt="Wersja"></a>
     <a href="https://pypi.org/project/vulcan-api/"><img src="https://img.shields.io/pypi/pyversions/vulcan-api.svg" alt="Wspierane wersje Python"></a>
-    <a href="https://travis-ci.org/github/HunterAzar/vulcan-api"><img src="https://travis-ci.org/HunterAzar/vulcan-api.svg?branch=master" alt="Status testów"></a>
+    <a href="https://travis-ci.com/kapi2289/vulcan-api"><img src="https://travis-ci.com/kapi2289/vulcan-api.svg?branch=master" alt="Status testów"></a>
     <a href="https://vulcan-api.readthedocs.io/pl/latest/?badge=latest"><img src="https://img.shields.io/readthedocs/vulcan-api.svg" alt="Status dokumentacji"></a>
 </p>
+<p align="center">
+    <a href="https://pypi.org/project/vulcan-api/"><img src="https://img.shields.io/pypi/dd/vulcan-api.svg" alt="Ilość pobrań dziennie"></a>
+    <a href="https://pypi.org/project/vulcan-api/"><img src="https://img.shields.io/pypi/dw/vulcan-api.svg" alt="Ilość pobrań tygodniowo"></a>
+    <a href="https://pypi.org/project/vulcan-api/"><img src="https://img.shields.io/pypi/dm/vulcan-api.svg" alt="Ilość pobrań miesięcznie"></a>
+</p>
+
+## Description
+
+Unofficial API for UONET+ e-register from [Vulcan](https://vulcan.edu.pl/). It's using the mobile API so you can register it as a mobile devide.
+
+![](https://raw.githubusercontent.com/kapi2289/vulcan-api/master/docs/source/_static/registered.png)
+
+## Installation
+
+You can install `vulcan-api` using `pip`
+
+```console
+$ python -m pip install vulcan-api
+```
+
+or you can build yourself the latest version
+
+```console
+$ git clone https://github.com/kapi2289/vulcan-api.git
+$ cd vulcan-api
+$ python -m pip install .
+```
+
+## Documentation
+
+You can find the documentation at https://vulcan-api.readthedocs.io/.
---
+++ _vulcan.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from ._api import Api
+from ._certificate import Certificate
+from ._exam import Exam
+from ._grade import Grade
+from ._homework import Homework
+from ._message import Message
+from ._lesson import Lesson
+from ._student import Student
+from ._utils import log
+
+
+class Vulcan:
+    """
+    Logs in to the e-register using generated certificate
+
+    Args:
+        certificate (:class:`dict`): Generated certificate
+            using :func:`vulcan.Vulcan.register`
+    """
+
+    def __init__(self, certificate, logging_level=None):
+        self._api = Api(certificate)
+
+        if logging_level:
+            Vulcan.set_logging_level(logging_level)
+
+        self.students = self.get_students()
+        self.set_student(next(self.students))
+
+    @staticmethod
+    def set_logging_level(logging_level):
+        """
+        Sets the default logging level
+
+        Args:
+            logging_level (:class:`int`): Logging level from :module:`logging` module
+        """
+
+        log.setLevel(logging_level)
+
+    @staticmethod
+    def register(token, symbol, pin):
+        """
+        Registers API as a new mobile device
+
+        Args:
+            token (:class:`str`): Token
+            symbol (:class:`str`): Symbol
+            pin (:class:`str`): PIN code
+
+        Returns:
+            :class:`dict`: Generated certificate, that you need to save
+        """
+        return Certificate.get(token, symbol, pin)
+
+    def get_students(self):
+        """
+        Yields students that are assigned to the account
+
+        Yields:
+            :class:`vulcan._student.Student`
+        """
+        return Student.get(self._api)
+
+    def set_student(self, student):
+        """
+        Sets the default student
+
+        Args:
+            student (:class:`vulcan._student.Student`): Student from :func:`vulcan.Vulcan.get_students`
+        """
+        self._api.set_student(student)
+
+    def get_grades(self):
+        """
+        Fetches student grades
+
+        Yields:
+            :class:`vulcan._grade.Grade`
+        """
+        return Grade.get(self._api)
+
+    def get_lessons(self, date=None):
+        """
+        Fetches lessons from the given date
+
+        Args:
+            date (:class:`datetime.date`): Date, from which to fetch lessons, if not provided
+                it's using the today date
+
+        Yields:
+            :class:`vulcan._lesson.Lesson`
+        """
+        return Lesson.get(self._api, date)
+
+    def get_exams(self, date=None):
+        """
+        Fetches exams from the given date
+
+        Args:
+            date (:class:`datetime.date`): Date, from which to fetch exams, if not provided
+                it's using the today date
+
+        Yields:
+            :class:`vulcan._exam.Exam`
+        """
+        return Exam.get(self._api, date)
+
+    def get_homework(self, date=None):
+        """
+        Fetches homework from the given date
+
+        Args:
+            date (:class:`datetime.date`): Date, from which to fetch exams, if not provided
+                it's using the today date
+
+        Yields:
+            :class:`vulcan._homework.Homework`
+        """
+        return Homework.get(self._api, date)
+
+    def get_messages(self, date=None):
+        """
+        Fetches messages from the given date
+
+        Args:
+            date (:class:`datetime.date`): Date, from which to fetch message, if not provided
+                it's using the today date
+
+        Yields:
+            :class:`vulcan._message.Message`
+        """
+        return Message.get(self._api, date)
+    """
+    def send_message(self, name, title, content, adresses):
+
+        
+        sends message to given adresses
+
+        Args:
+            title: Title of your message (required)
+            content: Content of your message
+            adressed: Adresses for message
+
+        Yields:
+            :class:`vulcan._message.Message`
+        
+        return Message.send(self._api, name, title, content, adresses)
+        """
