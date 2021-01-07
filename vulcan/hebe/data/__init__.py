ac94535f955d594da72c2200603d02e3fe196099
Czapla <56671347+Antoni-Czaplicki@users.noreply.github.com>
2021-01-07 12:23:44 +0100

    Update __init__.py

--- vulcan/hebe/data/__init__.py
+++ vulcan/hebe/data/__init__.py
@@ -2,6 +2,6 @@
 
 from ._exam import Exam
 from ._homework import Homework
-from ._lesson import Lesson, LessonTime, LessonRoom, LessonGroup
+from ._lesson import Lesson, LessonTime, LessonRoom
 from ._grade import Grade, GradeColumn, GradeCategory
 from ._lucky_number import LuckyNumber
