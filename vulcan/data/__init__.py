861d8286320363f7d607039572a32bcb02d6d051
Czapla <56671347+Antoni-Czaplicki@users.noreply.github.com>
2021-03-15 08:17:22 +0100

    Add LessonChanges

--- vulcan/data/__init__.py
+++ vulcan/data/__init__.py
@@ -5,5 +5,5 @@ from ._attendance import Attendance, PresenceType
 from ._exam import Exam
 from ._grade import Grade, GradeCategory, GradeColumn
 from ._homework import Homework
-from ._lesson import ChangedLesson, Lesson, LessonRoom
+from ._lesson import ChangedLesson, Lesson, LessonChanges, LessonRoom
 from ._lucky_number import LuckyNumber
