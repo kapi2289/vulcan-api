1756033eba0dc0dd80f95e011f48d0e22561be24
Antoni Czaplicki <56671347+Antoni-Czaplicki@users.noreply.github.com>
2022-05-04 20:54:06 +0200

    [Hotfix] Import missing exception in init (#110)
    * Import missing exception in init
    
    * Bump version

--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -3,6 +3,7 @@
 from ._account import Account
 from ._client import Vulcan
 from ._exceptions import (
+    ExpiredTokenException,
     InvalidPINException,
     InvalidSignatureValuesException,
     InvalidSymbolException,
@@ -12,13 +13,14 @@ from ._exceptions import (
 )
 from ._keystore import Keystore
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __doc__ = "Unofficial API for UONET+ e-register"
 
 __all__ = [
     "Vulcan",
     "Keystore",
     "Account",
+    "ExpiredTokenException",
     "InvalidPINException",
     "InvalidSignatureValuesException",
     "InvalidSymbolException",
