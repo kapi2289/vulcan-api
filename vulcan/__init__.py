97b11687d7586d3a247acdb8e5dc7363c211153d
Antoni-Czaplicki <antekczaplicki@gmail.com>
2022-05-04 16:38:01 +0200

    Import exceptions in __init__

--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -2,9 +2,27 @@
 
 from ._account import Account
 from ._client import Vulcan
+from ._exceptions import (
+    InvalidPINException,
+    InvalidSignatureValuesException,
+    InvalidSymbolException,
+    InvalidTokenException,
+    UnauthorizedCertificateException,
+    VulcanAPIException,
+)
 from ._keystore import Keystore
 
 __version__ = "2.1.0"
 __doc__ = "Unofficial API for UONET+ e-register"
 
-__all__ = ["Vulcan", "Keystore", "Account"]
+__all__ = [
+    "Vulcan",
+    "Keystore",
+    "Account",
+    "InvalidPINException",
+    "InvalidSignatureValuesException",
+    "InvalidSymbolException",
+    "InvalidTokenException",
+    "UnauthorizedCertificateException",
+    "VulcanAPIException",
+]
